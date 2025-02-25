c9d2a89bb5a43ca704d3eea2d80cd4aa97798f7a
Amaan-Zaidi4 <amaanzaidi443@gmail.com>
2025-02-25 15:37:25 +0530

    Add files via upload

---
+++ ASSIGNMENT 1/index.js
@@ -0,0 +1,86 @@
+// query 1 //
+
+let a = 2;
+let b= 5;
+let c= a+b;
+ console.log(a,b);
+ console.log(c);
+
+// query 2 //
+
+var A = 6;
+var B = 5;
+if(A<10 && B<10 || A>B && B>A){
+   console.log(true);
+   
+}
+else{
+    console.log(false);
+    
+}
+
+// query 3 //
+ 
+var A = 10;
+ var B = 2;
+ if(A/10 && B/10){
+    console.log(true);
+}
+else{
+    console.log(false);
+}
+if(A/10 || B/10){
+    console.log(true);
+    
+}
+
+// query 4 //
+
+var A = 9870;
+console.log(Math.floor (A/1000));
+
+
+// query 5 //
+ var A = 98709;
+ console.log(A%10);
+ 
+
+ // query 6 //
+ let a1 = 2;
+ let b1 = 9;
+ let c1 = b1%a1
+ console.log(a1,b1);
+ console.log(c1);
+
+ 
+ // query 7 //
+
+ let a2 = 2;
+ let b2 = 9;
+ let c2 = a2*b2
+ console.log(a2,b2);
+ console.log(c2);
+
+
+ // query 8 //
+ var maths= 50;
+ var english= 50;
+ var science= 50;
+ var sum= maths+english+science
+console.log(maths,english,science);
+console.log( "sum =",sum);
+console.log("average =", sum/3);
+
+ 
+
+ 
+
+
+ 
+
+
+ 
+
+
+ 
+ 
